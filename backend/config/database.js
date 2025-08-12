b69bdfd7cf8f593c5ab03dc3cdf2ef438c1a8fcb
WhatsScratchFinal <cristofer.contreras@technovalue.com.mx>
2025-08-12 16:49:10 -0600

    Versión inicial del proyecto para despliegue

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// La base de datos será un archivo llamado 'database.sqlite' en la carpeta del backend.
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-});
-
-module.exports = sequelize;
